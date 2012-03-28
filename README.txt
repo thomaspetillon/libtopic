e065d133f47497348f3a3d2e46aef94861da610f
Thomas Petillon <petillon@topic.fr>
2012-03-28 11:00:51 +0200

    created setup installer

---
+++ README.txt
@@ -0,0 +1,6 @@
+The libtopic library gathers several utility function for everyday use :
+* json helpers
+* regex helpers
+* mail helpers
+* list helpers
+...
---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = 'libtopic',
+    packages = ['libtopic'],
+    version = '1.2.0',    
+    author = 'Thomas Petillon',
+    author_email = 'petillon@topic.fr',
+    url = "https://github.com/tomjerry/libtopic",
+    description = 'Common utilities library',    
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development" 	
+        "Topic :: Utilities",
+    ]
+)
+
